1d433216a7ed9e8dd2f494c55f7a2654df80246b
besanh <anhle3532@mail.com>
2022-07-25 22:54:53 +0700

    page and route

---
+++ src/routes/about.svelte
@@ -0,0 +1,20 @@
+<script>
+</script>
+
+<div class="about">
+	<h2>About Us</h2>
+	<p>
+		Lorem ipsum dolor sit, amet consectetur adipisicing elit. In esse odit, architecto officia optio
+		itaque ratione, maiores iste id qui magnam? Odit delectus labore dolorem nisi tempora ullam, et
+		quo!
+	</p>
+	<div class="index">hello</div>
+</div>
+
+<style>
+	.about {
+		text-align: center;
+		display: block;
+		margin: 20px auto;
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    let title = "Ninja gaming guide"
+
+    const updateTitle = () => {
+        title = "New title"
+    }
+</script>
+
+<div class="index">
+	<h2>{title}</h2>
+	<p>
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae molestiae at fugit mollitia
+		consequatur ab culpa iure suscipit sapiente architecto, voluptates quasi corrupti corporis ut
+		praesentium odio doloribus nam laudantium.
+	</p>
+    <a href="/guides">Guides</a>
+    <a href="/about">About</a>
+</div>
+
+<style>
+    .index {
+        text-align: center;
+        display: block;
+        margin: 20px auto;
+    }
+</style>
